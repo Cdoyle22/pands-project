35b69584984bb3858e9161209d2c206aa5c01aac
Cdoyle22 <98051052+Cdoyle22@users.noreply.github.com>
2022-04-23 18:31:52 +0100

    Histograms

--- VariableToTextFile.py
+++ VariableToTextFile.py
@@ -5,9 +5,40 @@
 
 # Author: Ciara Doyle
 
-with open ("iris.csv") as f:
-    for line in f: 
-        print(line)
+import os
+
+print (os.chdir)
+
+import pandas as pd
+import numpy as np
+data = pd.read_csv ('iris.csv')
+
+x = data['sepal_length'].describe() # Define each variable and use describe function to get summary details
+y = data['sepal_width'].describe()
+z = data['petal_length'].describe()
+b = data['petal_width'].describe()
+c = data['class'].describe()
+
+filelocation = "C:/Users/paulb/Desktop/Programming and Scripting/Project/pands-project/summaryfile.txt" 
+
+print ("SUMMARY OF EACH VARIABLE IN THE IRIS DATASET: ", file = open (filelocation, "a")) # Write a heading to the text file
+print ('\r\n', file = open (filelocation, "a")) # Write a empty line to the text file (formatting purposes)
+print ('SEPAL LENGTH: ', file = open (filelocation, "a")) # Write the summary details of each variable to the text file
+print(x, file = open (filelocation, "a"))
+print ('\r\n', file = open (filelocation, "a"))
+print ('SEPAL WIDTH: ', file = open (filelocation, "a"))
+print(y, file = open (filelocation, "a"))
+print ('\r\n', file = open (filelocation, "a"))
+print ('PETAL LENGTH: ', file = open (filelocation, "a"))
+print(z, file = open (filelocation, "a"))
+print ('\r\n', file = open (filelocation, "a"))
+print ('PETAL WIDTH: ', file = open (filelocation, "a"))
+print(b, file = open (filelocation, "a"))
+print ('\r\n', file = open (filelocation, "a"))
+print ('CLASS: ', file = open (filelocation, "a"))
+print(c, file = open (filelocation, "a"))
+print ('\r\n', file = open (filelocation, "a"))
+
 
 
 
