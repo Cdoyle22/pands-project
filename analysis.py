eb341d047e73150448b03a978d1d6ea05f64cc0d
Cdoyle22 <98051052+Cdoyle22@users.noreply.github.com>
2022-04-24 18:53:53 +0100

    Other Analysis

--- analysis.py
+++ analysis.py
@@ -4,6 +4,10 @@
 # 3. Outputs a scatter plot of each pair of variables.
 # 4. Performs any other analysis you think is appropriate
 
+#4.1 Demonstrate the differences in each variable for each class of iris.
+#4.2 Write average petal length, width, sepal length, width of each class to a text file.
+
+
 
 #Author: Ciara Doyle
 
